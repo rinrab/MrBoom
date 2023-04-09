1774d45c9bda3be10e1714f2d3abc7ff0d62faf8
rinrab <tima@chemodax.net>
2023-04-09 16:55:39 +0300

    Add actvate event listener.

--- service-worker.js
+++ service-worker.js
@@ -53,6 +53,10 @@ self.addEventListener('install', (e) => {
     })());
 });
 
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
 // Fetching content using Service Worker
 self.addEventListener('fetch', (e) => {
     // Cache http and https only, skip unsupported chrome-extension:// and file://...

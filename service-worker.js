2a47b39fe215990f584e56c3bc396e2e3fabc8ce
rinrab <tima@chemodax.net>
2023-04-09 17:28:25 +0300

    Refactor code for fetch

--- service-worker.js
+++ service-worker.js
@@ -82,22 +82,22 @@ self.addEventListener('activate', event => {
 });
 
 // Fetching content using Service Worker
-self.addEventListener('fetch', (e) => {
+self.addEveself.addEventListener('fetch', (e) => {
     // Cache http and https only, skip unsupported chrome-extension:// and file://...
-    if (!(
-        e.request.url.startsWith('http:') || e.request.url.startsWith('https:')
-    )) {
-        return;
+    if (e.request.url.startsWith('http:') || e.request.url.startsWith('https:')) {
+        e.respondWith((async () => {
+            const r = await caches.match(e.request);
+            if (r) {
+                console.log(`[Service Worker] Serving ${e.request.url} from cache.`);
+                return r;
+            } else {
+                console.warn(`[Service Worker] Serving ${e.request.url} from server.`);
+                const response = await fetch(e.request);
+                const cache = await caches.open(cacheName);
+                console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+                cache.put(e.request, response.clone());
+                return response;
+            }
+        })());
     }
-
-    e.respondWith((async () => {
-        const r = await caches.match(e.request);
-        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-        if (r) return r;
-        const response = await fetch(e.request);
-        const cache = await caches.open(cacheName);
-        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-        cache.put(e.request, response.clone());
-        return response;
-    })());
 });

b1ab1922f9516ab853b0c43c1cc5f5742e7b7060
rinrab <tima@chemodax.net>
2023-07-10 15:14:17 +0300

    Revert "."
    This reverts commit cc0d7ba53c39599e41e159b49e0b8e9a7aef0a54.

--- service-worker.js
+++ service-worker.js
@@ -19,19 +19,21 @@ self.addEventListener('activate', event => {
 // Fetching content using Service Worker
 self.addEventListener('fetch', (e) => {
     // Cache http and https only, skip unsupported chrome-extension:// and file://...
-    e.respondWith((async () => {
-        const cache = await caches.open(cacheName);
-        const r = await caches.match(e.request);
-        if (r) {
-            console.log(`[Service Worker] Serving ${e.request.url} from cache.`);
-            return r;
-        } else {
-            console.warn(`[Service Worker] Serving ${e.request.url} from server.`);
-            const response = await fetch(e.request);
-            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    if (e.request.url.startsWith('http:') || e.request.url.startsWith('https:')) {
+        e.respondWith((async () => {
+            const cache = await caches.open(cacheName);
+            const r = await caches.match(e.request);
+            if (r) {
+                console.log(`[Service Worker] Serving ${e.request.url} from cache.`);
+                return r;
+            } else {
+                console.warn(`[Service Worker] Serving ${e.request.url} from server.`);
+                const response = await fetch(e.request);
+                console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
 
-            // cache.put(e.request, response.clone());
-            return response;
-        }
-    })());
+                // cache.put(e.request, response.clone());
+                return response;
+            }
+        })());
+    }
 });

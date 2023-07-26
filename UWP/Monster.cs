8413f69a7c66cbd49b25169bb765ca96d29443c5
chemodax <ivan@zhakov.com>
2023-07-26 17:07:24 +0300

    Cleanup usings.

--- UWP/Monster.cs
+++ UWP/Monster.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using static MrBoom.Assets;
 
 namespace MrBoom
 {
--- UWP/Terrain.cs
+++ UWP/Terrain.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using static MrBoom.Assets;
 
 namespace MrBoom
 {

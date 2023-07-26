c8f62aae1ad95721fe6c7a369d63f8c488f49462
chemodax <ivan@zhakov.com>
2023-07-26 19:41:02 +0300

    Cleanup usings.

--- UWP/Assets.cs
+++ UWP/Assets.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Windows.Graphics.Display;
-using Windows.UI.Xaml.Controls.Maps;
 
 namespace MrBoom
 {

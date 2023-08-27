470fd56ad6c2f82356efbe97377e8293470dbba2
rinrab <tima@chemodax.net>
2023-08-27 15:21:57 +0300

    Move class Team to Team.cs

--- UWP/PlayerState.cs
+++ UWP/PlayerState.cs
@@ -1,7 +1,5 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
-using System.Collections.Generic;
-
 namespace MrBoom
 {
     public class PlayerState
@@ -15,37 +13,6 @@ namespace MrBoom
         {
             Controller = controller;
             Index = index;
-        }
-    }
-
-    public class Team
-    {
-        public static int Mode;
-
-        public List<PlayerState> Players;
-        public int VictoryCount;
-
-        public string[] Names
-        {
-            get
-            {
-                List<string> names = new List<string>();
-                foreach (PlayerState player in Players)
-                {
-                    names.Add(player.Name);
-                }
-                return names.ToArray();
-            }
-        }
-
-        public static PlayerState[] GetPlayers(IEnumerable<Team> teams)
-        {
-            List<PlayerState> players = new List<PlayerState>();
-            foreach (Team team in teams)
-            {
-                players.AddRange(team.Players);
-            }
-            return players.ToArray();
         }
     }
 }
---
+++ UWP/Team.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System.Collections.Generic;
+
+namespace MrBoom
+{
+    public class Team
+    {
+        public static int Mode;
+
+        public List<PlayerState> Players;
+        public int VictoryCount;
+
+        public string[] Names
+        {
+            get
+            {
+                List<string> names = new List<string>();
+                foreach (PlayerState player in Players)
+                {
+                    names.Add(player.Name);
+                }
+                return names.ToArray();
+            }
+        }
+
+        public static PlayerState[] GetPlayers(IEnumerable<Team> teams)
+        {
+            List<PlayerState> players = new List<PlayerState>();
+            foreach (Team team in teams)
+            {
+                players.AddRange(team.Players);
+            }
+            return players.ToArray();
+        }
+    }
+}

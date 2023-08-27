3cabf0827ea99e5008144f83198849a48fb8af77
rinrab <tima@chemodax.net>
2023-08-27 15:57:31 +0300

    Remove using

--- UWP/MultiplayerStartScreen.cs
+++ UWP/MultiplayerStartScreen.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using SharpDX.Direct2D1.Effects;
 using Windows.ApplicationModel;
 using Windows.UI.Xaml;
 

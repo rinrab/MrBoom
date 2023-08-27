ce61e2a705fe247734a78f18c1df12f3653b37c0
rinrab <tima@chemodax.net>
2023-08-27 17:02:07 +0300

    Clean up

--- UWP/StartScreen.cs
+++ UWP/StartScreen.cs
@@ -3,7 +3,6 @@
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using Windows.UI.Xaml;
 
 namespace MrBoom

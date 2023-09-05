2b8db748d747061537ac97c5f9e504136007c903
rinrab <tima@chemodax.net>
2023-09-05 18:23:36 +0200

    Add bot correctly.

--- UWP/PlayerState.cs
+++ UWP/PlayerState.cs
@@ -4,15 +4,24 @@ namespace MrBoom
 {
     public class PlayerState
     {
+        public enum Type
+        {
+            Human,
+            Bot
+        }
+
         public IController Controller;
         public string Name;
         public int VictoryCount;
         public int Index;
+        public Type type;
 
-        public PlayerState(IController controller, int index)
+        public PlayerState(IController controller, int index, Type type, string name)
         {
             Controller = controller;
             Index = index;
+            this.type = type;
+            Name = name;
         }
     }
 }

5972bc8152e74b95f8a80385e846269e9d2a0b28
Ivan.Z <53048645+chemodax@users.noreply.github.com>
2023-09-09 19:09:54 +0400

    Add Random.Shuffle() extension.

---
+++ UWP/RandomExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+
+namespace MrBoom
+{
+    public static class RandomExtensions
+    {
+        public static void Shuffle<T>(this Random random, IList<T> items)
+        {
+            int count = items.Count;
+
+            for (int i = 0; i < count - 1; i++)
+            {
+                int j = random.Next(i, count);
+
+                if (j != i)
+                {
+                    T temp = items[i];
+                    items[i] = items[j];
+                    items[j] = temp;
+                }
+            }
+        }
+    }
+}

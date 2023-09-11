5e33a891790acc30f8c2b04393d1f6ef558b6ade
rinrab <tima@chemodax.net>
2023-09-11 20:38:50 +0200

    Move ActionNode to file in BehaviorTree.

---
+++ UWP/BehaviorTree/ActionNode.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+using MrBoom.BehaviorTree;
+
+namespace MrBoom.Bot
+{
+    class ActionNode : BtNode
+    {
+        public delegate BtStatus ActionlDelegate();
+
+        readonly ActionlDelegate action;
+        private readonly bool wait;
+        private bool done;
+
+        public ActionNode(ActionlDelegate action, bool wait = false)
+        {
+            this.action = action;
+            this.wait = wait;
+        }
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+            done = false;
+        }
+
+        protected override BtStatus OnUpdate()
+        {
+            BtStatus status = action();
+            if (status == BtStatus.Success && wait && !done)
+            {
+                done = true;
+                return BtStatus.Running;
+            }
+
+            return status;
+        }
+    }
+}
--- UWP/Bot/ComputerPlayer.cs
+++ UWP/Bot/ComputerPlayer.cs
@@ -6,39 +6,6 @@ using MrBoom.BehaviorTree;
 
 namespace MrBoom.Bot
 {
-    class ActionNode : BtNode
-    {
-        public delegate BtStatus ActionlDelegate();
-
-        readonly ActionlDelegate action;
-        private readonly bool wait;
-        private bool done;
-
-        public ActionNode(ActionlDelegate action, bool wait = false)
-        {
-            this.action = action;
-            this.wait = wait;
-        }
-
-        protected override void OnInitialize()
-        {
-            base.OnInitialize();
-            done = false;
-        }
-
-        protected override BtStatus OnUpdate()
-        {
-            BtStatus status = action();
-            if (status == BtStatus.Success && wait && !done)
-            {
-                done = true;
-                return BtStatus.Running;
-            }
-
-            return status;
-        }
-    }
-
     public class ComputerPlayer : AbstractPlayer
     {
         private BtSelector tree;

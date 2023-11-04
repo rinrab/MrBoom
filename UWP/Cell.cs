ac675a7cc011a22454708cf2903ddf66995f9a36
rinrab <tima@chemodax.net>
2023-11-04 11:17:21 +0200

    Move class Cell to Cell.cs

---
+++ UWP/Cell.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Timofei Zhakov. All rights reserved.
+
+namespace MrBoom
+{
+    public class Cell
+    {
+        public readonly TerrainType Type;
+        public AnimatedImage Images;
+        public int Index;
+        public int animateDelay;
+        public int bombCountdown;
+        public int maxBoom;
+        public bool rcAllowed;
+        public AbstractPlayer owner;
+        public Cell Next;
+        public PowerUpType PowerUpType;
+        public int OffsetX;
+        public int OffsetY;
+        public int DeltaX;
+        public int DeltaY;
+
+        public Cell(TerrainType type)
+        {
+            Type = type;
+            Index = -1;
+        }
+    }
+}
--- UWP/Terrain.cs
+++ UWP/Terrain.cs
@@ -730,30 +730,6 @@ namespace MrBoom
         }
     }
 
-    public class Cell
-    {
-        public readonly TerrainType Type;
-        public AnimatedImage Images;
-        public int Index;
-        public int animateDelay;
-        public int bombCountdown;
-        public int maxBoom;
-        public bool rcAllowed;
-        public AbstractPlayer owner;
-        public Cell Next;
-        public PowerUpType PowerUpType;
-        public int OffsetX;
-        public int OffsetY;
-        public int DeltaX;
-        public int DeltaY;
-
-        public Cell(TerrainType type)
-        {
-            Type = type;
-            Index = -1;
-        }
-    }
-
     public enum PowerUpType
     {
         Banana,

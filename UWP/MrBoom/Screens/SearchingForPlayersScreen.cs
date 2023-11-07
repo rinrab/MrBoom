ae7784e446c2ffd7a7457d754e2845a019820b31
rinrab <tima@chemodax.net>
2023-11-07 15:41:18 +0100

    Added SearchingForPlayers Screen.

--- UWP/MrBoom/Screens/SearchingForPlayersScreen.cs
+++ UWP/MrBoom/Screens/SearchingForPlayersScreen.cs
@@ -1,47 +1,48 @@
 ﻿// Copyright (c) Timofei Zhakov. All rights reserved.
 
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace MrBoom
 {
-    public class OnlineStartScreen : IScreen
+    public class SearchingForPlayersScreen : IScreen
     {
+        private Task matchmakingTask;
         private readonly Assets assets;
         private readonly List<Team> teams;
         private readonly List<IController> controllers;
         private readonly Settings settings;
+        private readonly IController currentPlayer;
 
         public Screen Next => Screen.None;
 
-        public OnlineStartScreen(Assets assets, List<Team> teams, List<IController> controllers, Settings settings)
+        public SearchingForPlayersScreen(Assets assets, List<Team> teams, List<IController> controllers,
+                                         Settings settings, IController currentPlayer)
         {
+            matchmakingTask = StartMatchmaking();
             this.assets = assets;
             this.teams = teams;
             this.controllers = controllers;
             this.settings = settings;
+            this.currentPlayer = currentPlayer;
+        }
+
+        private async Task StartMatchmaking()
+        {
+            // TODO:
         }
 
         public void Update()
         {
-            foreach (IController controller in controllers)
-            {
-                if (controller.IsKeyDown(PlayerKeys.Bomb))
-                {
-                    assets.Sounds.Bang.Play();
-                    ScreenManager.SetScreen(new SearchingForPlayersScreen(assets, teams, controllers, settings, controller));
-                    break;
-                }
-            }
         }
 
         public void Draw(SpriteBatch ctx)
         {
             assets.MrFond.Draw(ctx, 0, 0);
-            string text = "push   !!";
-            assets.Controls[0].Draw(ctx, 320 / 2, 181);
-            Game.DrawString(ctx, (320 - text.Length * 8) / 2, 186, text, assets.Alpha[1]);
+            string text = "searching for players...";
+            Game.DrawString(ctx, (320 - text.Length * 8) / 2, 190, text, assets.Alpha[1]);
         }
 
         public void DrawHighDPI(SpriteBatch ctx, Rectangle rect, float scale, int graphicScale)

d06c3b450f4f2eacc227ad49363964cca4a64631
rinrab <tima@chemodax.net>
2023-11-14 15:31:59 +0100

    Add connection.

--- UWP/MrBoom/Screens/SearchingForPlayersScreen.cs
+++ UWP/MrBoom/Screens/SearchingForPlayersScreen.cs
@@ -2,24 +2,22 @@
 
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using PlayFab;
-using PlayFab.ClientModels;
-using PlayFab.MultiplayerModels;
+using Newtonsoft.Json;
 
 namespace MrBoom
 {
     public class SearchingForPlayersScreen : IScreen
     {
-        private Task matchmakingTask;
         private readonly Assets assets;
         private readonly List<Team> teams;
         private readonly List<IController> controllers;
         private readonly Settings settings;
         private readonly IController currentPlayer;
-        private readonly NameGenerator nameGenerator;
         private string status;
 
         public Screen Next => Screen.None;
@@ -32,111 +30,31 @@ namespace MrBoom
             this.controllers = controllers;
             this.settings = settings;
             this.currentPlayer = currentPlayer;
-            this.nameGenerator = new NameGenerator(Terrain.Random);
+            status = "finding room";
 
-            matchmakingTask = StartMatchmaking();
+            Task.Run(() => StartMatchmaking());
         }
 
         private async Task StartMatchmaking()
         {
-            PlayFabSettings.staticSettings.TitleId = "E8B53";
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5191");
 
-            string playerId = settings.PlayerId;
-            if (playerId == null)
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(new
             {
-                Guid guid = Guid.NewGuid();
-                playerId = guid.ToString("N");
-                settings.PlayerId = playerId;
-            }
+                Name = new NameGenerator(Terrain.Random).GenerateName(),
+            }));
 
-            if (settings.IsDebug)
-            {
-                playerId += "-debug";
-            }
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
-            status = "logging in";
+            HttpResponseMessage res = await client.PostAsync("/room/connect", content);
 
-            PlayFabResult<LoginResult> login = await PlayFabClientAPI.LoginWithCustomIDAsync(new LoginWithCustomIDRequest
-            {
-                CustomId = playerId,
-                CreateAccount = true
-            });
+            Room room = JsonConvert.DeserializeObject<Room>(await res.Content.ReadAsStringAsync());
 
-            await PlayFabMultiplayerAPI.CancelAllMatchmakingTicketsForPlayerAsync(new CancelAllMatchmakingTicketsForPlayerRequest
-            {
-                Entity = new PlayFab.MultiplayerModels.EntityKey
-                {
-                    Id = login.Result.EntityToken.Entity.Id,
-                    Type = login.Result.EntityToken.Entity.Type,
-                },
-                QueueName = "default",
-            });
+            MultiplayerService multiplayerService = new MultiplayerService(room.Hostname, room.Port);
+            multiplayerService.StartListen();
 
-            //if (login.Result.NewlyCreated)
-            //{
-            //    await PlayFabClientAPI.UpdateUserTitleDisplayNameAsync(new UpdateUserTitleDisplayNameRequest
-            //    {
-            //        DisplayName = new NameGenerator(Terrain.Random).GenerateName()
-            //    });
-            //}
-
-            status = "creating ticket";
-
-            MultiplayerService multiplayerService = new MultiplayerService();
-            string ip = multiplayerService.GetLocalIPAddress();
-            int port = Terrain.Random.Next(65535);
-            multiplayerService.StartListen(port);
-
-            PlayFabResult<CreateMatchmakingTicketResult> ticket = await PlayFabMultiplayerAPI.CreateMatchmakingTicketAsync(
-                new CreateMatchmakingTicketRequest
-                {
-                    Creator = new MatchmakingPlayer
-                    {
-                        Entity = new PlayFab.MultiplayerModels.EntityKey
-                        {
-                            Id = login.Result.EntityToken.Entity.Id,
-                            Type = login.Result.EntityToken.Entity.Type,
-                        },
-                        Attributes = new MatchmakingPlayerAttributes
-                        {
-                            DataObject = new
-                            {
-                                Name = new NameGenerator(Terrain.Random).GenerateName(),
-                                Ip = ip,
-                                Port = port
-                            }
-                        },
-                    },
-                    GiveUpAfterSeconds = 120,
-                    QueueName = "default",
-                });
-
-            while (true)
-            {
-                PlayFabResult<GetMatchmakingTicketResult> newTicket = await PlayFabMultiplayerAPI.GetMatchmakingTicketAsync(new GetMatchmakingTicketRequest
-                {
-                    TicketId = ticket.Result.TicketId,
-                    QueueName = "default"
-                });
-
-                if (newTicket.Result.Status == "Matched")
-                {
-                    PlayFabResult<GetMatchResult> match = await PlayFabMultiplayerAPI.GetMatchAsync(new GetMatchRequest
-                    {
-                        MatchId = newTicket.Result.MatchId,
-                        QueueName = "default",
-                        ReturnMemberAttributes = true,
-                    });
-
-                    ScreenManager.SetScreen(new OnlinePlayerListScreen(assets, teams, controllers, settings, currentPlayer, match, multiplayerService));
-
-                    break;
-                }
-
-                status = newTicket.Result.Status;
-
-                await Task.Delay(6000);
-            }
+            ScreenManager.SetScreen(new OnlinePlayerListScreen(assets, teams, controllers, settings, currentPlayer, multiplayerService));
         }
 
         public void Update()
@@ -170,5 +88,12 @@ namespace MrBoom
         public void DrawHighDPI(SpriteBatch ctx, Rectangle rect, float scale, int graphicScale)
         {
         }
+
+        private class Room
+        {
+            public string Id { get; set; }
+            public string Hostname { get; set; }
+            public int Port { get; set; }
+        };
     }
 }
